eb8f6ff31be4aea9e98ffd9697837308498eed10
hk123 <hk123@live.co.uk>
2024-04-28 01:05:24 +0100

    Added unit tests for CustomerInfoToDisplayAsync

---
+++ AppointmentBooking.Tests/Customers/CustomerService/CustomerInfoToDisplayTests.cs
@@ -0,0 +1,74 @@
+﻿namespace AppointmentBooking.Tests.Customers.CustomerService;
+
+using AppointmentBooking.Context.Models;
+using AppointmentBooking.Customers;
+using AppointmentBooking.Customers.DTO;
+using AutoFixture;
+using AutoFixture.Xunit2;
+using Moq;
+
+public class CustomerInfoToDisplayTests : BaseTest
+{
+    private readonly ICustomerService _customerService;
+
+    public CustomerInfoToDisplayTests()
+    {
+        _customerService = fixture.Create<CustomerService>();
+    }
+
+    [Theory, AutoData]
+    public async Task CustomerInfoToDisplayAsync_KnownCustomerIdPassedIn_ReturnsCustomerObject(int customerId, string customerName, string phoneNumber, string emailAddress, string companyName)
+    {
+        // arrange
+        Customer customer = fixture
+            .Build<Customer>()
+            .Without(c => c.Meetings)
+            .With(c => c.Id, customerId)
+            .With(c => c.Name, customerName)
+            .With(c => c.PhoneNumber, phoneNumber)
+            .With(c => c.EmailAddress, emailAddress)
+            .With(c => c.CompanyName, companyName)
+            .Create();
+
+        context.Add(customer);
+        context.SaveChanges();
+
+        // act
+        CustomerResponse? customerResponse = await _customerService
+            .CustomerInfoToDisplayAsync(customerId)
+            .ConfigureAwait(false);
+
+        // assert
+        Assert.Equal(customerId, customerResponse?.Id);
+        Assert.Equal(customerName, customerResponse?.Name);
+        Assert.Equal(phoneNumber, customerResponse?.PhoneNumber);
+        Assert.Equal(emailAddress, customerResponse?.EmailAddress);
+        Assert.Equal(companyName, customerResponse?.CompanyName);
+    }
+
+    [Theory, AutoData]
+    public async Task CustomerInfoToDisplayAsync_UnknownCustomerIdPassedIn_ReturnsNull(int customerId, string customerName, string phoneNumber, string emailAddress, string companyName)
+    {
+        // arrange
+        Customer customer = fixture
+            .Build<Customer>()
+            .Without(c => c.Meetings)
+            .With(c => c.Id, It.IsAny<int>())
+            .With(c => c.Name, customerName)
+            .With(c => c.PhoneNumber, phoneNumber)
+            .With(c => c.EmailAddress, emailAddress)
+            .With(c => c.CompanyName, companyName)
+            .Create();
+
+        context.Add(customer);
+        context.SaveChanges();
+
+        // act
+        CustomerResponse? customerResponse = await _customerService
+            .CustomerInfoToDisplayAsync(customerId)
+            .ConfigureAwait(false);
+
+        // assert
+        Assert.Null(customerResponse);
+    }
+}
--- AppointmentBooking/Customers/CustomerService.cs
+++ AppointmentBooking/Customers/CustomerService.cs
@@ -1,116 +1,115 @@
-﻿namespace AppointmentBooking.Customers
+﻿namespace AppointmentBooking.Customers;
+
+using AppointmentBooking.Context;
+using AppointmentBooking.Context.Models;
+using AppointmentBooking.Customers.DTO;
+using Microsoft.EntityFrameworkCore;
+using static AppointmentBooking.Customers.ICustomerService;
+
+public class CustomerService : ICustomerService
 {
-    using AppointmentBooking.Context;
-    using AppointmentBooking.Context.Models;
-    using AppointmentBooking.Customers.DTO;
-    using Microsoft.EntityFrameworkCore;
-    using static AppointmentBooking.Customers.ICustomerService;
+    private readonly ILogger<ICustomerService> _logger;
+    private readonly AppointmentBookingDbContext _context;
 
-    public class CustomerService : ICustomerService
+    public CustomerService(
+        ILogger<ICustomerService> logger,
+        AppointmentBookingDbContext context)
     {
-        private readonly ILogger<ICustomerService> _logger;
-        private readonly AppointmentBookingDbContext _context;
+        _logger = logger;
+        _context = context;
+    }
 
-        public CustomerService(
-            ILogger<ICustomerService> logger,
-            AppointmentBookingDbContext context)
+    public async Task<Customer?> GetCustomerAsync(int id) =>
+        await _context.Customers.AsNoTracking().SingleOrDefaultAsync(c => c.Id == id).ConfigureAwait(false);
+
+    public async Task<Customer?> GetCustomerTrackingAsync(int id) =>
+        await _context.Customers.SingleOrDefaultAsync(c => c.Id == id).ConfigureAwait(false);
+
+    public async Task<CustomerResponse?> CustomerInfoToDisplayAsync(int id)
+    {            
+        Customer? customer = await this.GetCustomerAsync(id).ConfigureAwait(false);
+
+        if (customer != null) 
         {
-            _logger = logger;
-            _context = context;
+            return new CustomerResponse()
+            {
+                Id = customer.Id,
+                Name = customer.Name,
+                PhoneNumber = customer.PhoneNumber,
+                EmailAddress = customer.EmailAddress,
+                CompanyName = customer.CompanyName,
+            };
         }
 
-        private async Task<Customer?> GetCustomerAsync(int id) =>
-            await _context.Customers.AsNoTracking().SingleOrDefaultAsync(c => c.Id == id).ConfigureAwait(false);
+        return null;
+    }
 
-        private async Task<Customer?> GetCustomerTrackingAsync(int id) =>
-            await _context.Customers.SingleOrDefaultAsync(c => c.Id == id).ConfigureAwait(false);
+    public async Task<List<Customer>?> GetCustomersAsync(int[] ids) =>
+        await _context.Customers.AsNoTracking().Where(c => ids.Contains(c.Id)).ToListAsync();
 
-        public async Task<CustomerResponse?> CustomerInfoToDisplayAsync(int id)
-        {            
-            Customer? customer = await this.GetCustomerAsync(id).ConfigureAwait(false);
+    public async Task<List<Customer>?> GetAllCustomersAsync() =>
+        await _context.Customers.AsNoTracking().ToListAsync();
 
-            if (customer != null) 
+    public async Task<bool> CreateCustomerAsync(CreateCustomerRequest request)
+    {
+        int entriesSaved;
+
+        if (request != null)
+        {
+            Customer customer = new()
             {
-                return new CustomerResponse()
-                {
-                    Id = customer.Id,
-                    Name = customer.Name,
-                    PhoneNumber = customer.PhoneNumber,
-                    EmailAddress = customer.EmailAddress,
-                    CompanyName = customer.CompanyName,
-                };
-            }
+                Name = request.Name,
+                EmailAddress = request.EmailAddress,
+                PhoneNumber = request.PhoneNumber,
+                CompanyName = request.CompanyName,
+            };
 
-            return null;
+            _context.Customers.Add(customer);
+            entriesSaved = await _context.SaveChangesAsync().ConfigureAwait(false);
+        }
+        else
+        {
+            _logger.LogError("One of the following are null, Name: {name}, Email Address: {email} or Phone Number: {phoneNumber}",
+                             request?.Name,
+                             request?.EmailAddress,
+                             request?.PhoneNumber);
+
+            throw new ArgumentNullException(nameof(request));
         }
 
-        public async Task<List<Customer>?> GetCustomersAsync(int[] ids) =>
-            await _context.Customers.AsNoTracking().Where(c => ids.Contains(c.Id)).ToListAsync();
+        bool isCustomerSaved = entriesSaved >= 1;
+        return isCustomerSaved;
+    }
 
-        public async Task<List<Customer>?> GetAllCustomersAsync() =>
-            await _context.Customers.AsNoTracking().ToListAsync();
+    public async Task<bool> UpdateCustomerAsync(UpdateCustomerRequest request)
+    {
+        Customer? customer = await this.GetCustomerTrackingAsync(request.Id).ConfigureAwait(false);
 
-        public async Task<bool> CreateCustomerAsync(CreateCustomerRequest request)
+        if (customer != null)
         {
-            int entriesSaved;
-
-            if (request != null)
-            {
-                Customer customer = new()
-                {
-                    Name = request.Name,
-                    EmailAddress = request.EmailAddress,
-                    PhoneNumber = request.PhoneNumber,
-                    CompanyName = request.CompanyName,
-                };
-
-                _context.Customers.Add(customer);
-                entriesSaved = await _context.SaveChangesAsync().ConfigureAwait(false);
-            }
-            else
-            {
-                _logger.LogError("One of the following are null, Name: {name}, Email Address: {email} or Phone Number: {phoneNumber}",
-                                 request?.Name,
-                                 request?.EmailAddress,
-                                 request?.PhoneNumber);
-
-                throw new ArgumentNullException(nameof(request));
-            }
-
-            bool isCustomerSaved = entriesSaved >= 1;
-            return isCustomerSaved;
+            customer.Id = request.Id;
+            customer.Name = request.Name;
+            customer.PhoneNumber = request.PhoneNumber;
+            customer.EmailAddress = request.EmailAddress;
+            customer.CompanyName = request.CompanyName;
         }
 
-        public async Task<bool> UpdateCustomerAsync(UpdateCustomerRequest request)
+        int entriesSaved = await _context.SaveChangesAsync().ConfigureAwait(false);
+        return entriesSaved >= 1;
+    }
+
+    public async Task<bool> DeleteCustomerAsync(int Id)
+    {
+        Customer? customer = await this.GetCustomerAsync(Id).ConfigureAwait(false);
+
+        if (customer != null)
         {
-            Customer? customer = await this.GetCustomerTrackingAsync(request.Id).ConfigureAwait(false);
-
-            if (customer != null)
-            {
-                customer.Id = request.Id;
-                customer.Name = request.Name;
-                customer.PhoneNumber = request.PhoneNumber;
-                customer.EmailAddress = request.EmailAddress;
-                customer.CompanyName = request.CompanyName;
-            }
-
+            _context.Remove(customer);
             int entriesSaved = await _context.SaveChangesAsync().ConfigureAwait(false);
+
             return entriesSaved >= 1;
         }
-
-        public async Task<bool> DeleteCustomerAsync(int Id)
-        {
-            Customer? customer = await this.GetCustomerAsync(Id).ConfigureAwait(false);
-
-            if (customer != null)
-            {
-                _context.Remove(customer);
-                int entriesSaved = await _context.SaveChangesAsync().ConfigureAwait(false);
-
-                return entriesSaved >= 1;
-            }
-            
-            return false;
-        }
+        
+        return false;
     }
 }
