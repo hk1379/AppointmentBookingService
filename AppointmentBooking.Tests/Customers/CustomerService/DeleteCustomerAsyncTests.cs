27b6213fb0bda82682fb74bfa168c1451ce8ede8
hk123 <hk123@live.co.uk>
2024-05-05 02:18:35 +0100

    Added tests for DeleteCustomerAsync

---
+++ AppointmentBooking.Tests/Customers/CustomerService/DeleteCustomerAsyncTests.cs
@@ -0,0 +1,61 @@
+﻿namespace AppointmentBooking.Tests.Customers.CustomerService;
+
+using AppointmentBooking.Context.Models;
+using AppointmentBooking.Customers.Requests;
+using AppointmentBooking.Customers;
+using AutoFixture.Xunit2;
+using AutoFixture;
+public class DeleteCustomerAsyncTests : BaseTest
+{
+    private readonly ICustomerService _customerService;
+
+    public DeleteCustomerAsyncTests()
+    {
+        _customerService = fixture.Create<CustomerService>();
+    }
+
+    private void CreateAndSaveCustomer(string name, string phoneNumber, string emailAddress, string companyName)
+    {
+        Customer customer = new()
+        {
+            Name = name,
+            EmailAddress = phoneNumber,
+            PhoneNumber = emailAddress,
+            CompanyName = companyName,
+        };
+        context.Add(customer);
+        context.SaveChanges();
+    }
+
+    [Theory]
+    [InlineData(1)]
+    public async Task DeleteCustomerAsync_ValidCustomerIdGiven_ReturnsTrue(int id)
+    {
+        // arrange
+        CreateAndSaveCustomer(fixture.Create<string>(), fixture.Create<string>(), fixture.Create<string>(), fixture.Create<string>());
+
+        // act
+        bool customerDeleted = await _customerService
+            .DeleteCustomerAsync(id)
+            .ConfigureAwait(false);
+
+        // assert
+        Assert.True(customerDeleted);
+    }
+
+    [Theory]
+    [InlineData(100)]
+    public async Task DeleteCustomerAsync_UnknownCustomerIdGiven_ReturnsFalse(int id)
+    {
+        // arrange
+        CreateAndSaveCustomer(fixture.Create<string>(), fixture.Create<string>(), fixture.Create<string>(), fixture.Create<string>());
+
+        // act
+        bool customerDeleted = await _customerService
+            .DeleteCustomerAsync(id)
+            .ConfigureAwait(false);
+
+        // assert
+        Assert.False(customerDeleted);
+    }
+}
